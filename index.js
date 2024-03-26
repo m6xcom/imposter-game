cbc47c9741c4c0eae3a209eeda41869a81eb4e3e
Max <max@webmaestro.com.ua>
2024-03-26 12:11:54 +0200

    adds game

---
+++ index.js
@@ -0,0 +1,46 @@
+const playersCountConfirmButton = document.getElementsByClassName('js--confirm-count')[0];
+const playerBlockTemplate = document.getElementById('playerBlock').content.firstElementChild;
+const playerBlocksWrapper = document.getElementsByClassName('js--player-blocks')[0];
+
+let word = '';
+
+fetch('./words.json').then(async resp=>{
+    const data = await resp.json();
+    word = data[Math.floor(Math.random() * data.length)];
+})
+
+
+
+function renderPlayerBlock(playersCount){
+    const playerWhoDoesntKnowWord = Math.floor(Math.random() * playersCount);
+    for(let i = 1; i <= playersCount; i++){
+        const playerBlock = playerBlockTemplate.cloneNode(true);
+        if(i === 1) playerBlock.classList.remove('hide');
+        playerBlock.querySelector('.js--player-number').innerHTML = i;
+        playerBlock.querySelector('.js--your-word').innerHTML = i===playerWhoDoesntKnowWord ? '-' : word;
+        playerBlocksWrapper.insertAdjacentElement('beforeend', playerBlock);
+    }
+    const readyButtons = document.getElementsByClassName('js--player-ready');
+    Array.from(readyButtons, button=>{
+        button.addEventListener('click', ()=>{
+            button.closest('.player-block__step').classList.add('hide');
+        })
+    });
+    const nextPlayerButtons = document.getElementsByClassName('js--next-player');
+    Array.from(nextPlayerButtons, button=>{
+        button.addEventListener('click', ()=>{
+            const playerBlock = button.closest('.player-block');
+            playerBlock.classList.add('hide');
+            playerBlock.nextElementSibling ? playerBlock.nextElementSibling.classList.remove('hide') : window.location.reload();
+        })
+    })
+}
+
+playersCountConfirmButton.addEventListener('click', ()=>{
+    const playersCountInput = document.getElementsByClassName('js--players-count')[0];
+    if(Number(playersCountInput.value) === NaN){
+        return;
+    }
+    playersCountConfirmButton.parentElement.classList.add('hide');
+    renderPlayerBlock(playersCountInput.value);
+});
